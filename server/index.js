400f44f9e2c95eda7bf400b64280ebcdac5b702f
Daniel Godfrey <dgodfrey17@gmail.com>
2022-04-14 18:12:57 -0700

    establish foundation of app - dependencies plus configs

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/../dist'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.listen(port, () => {
+  console.log(`Vector app listening on port ${port}`)
+})
